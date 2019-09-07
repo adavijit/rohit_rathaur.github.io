f7b33181717b4ab3f4d5f2b010d175e90860b515
glob4lh3ll <54185582+glob4lh3ll@users.noreply.github.com>
2019-09-07 19:27:50 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 function loadSkills(skills){
     var i=0,j;
     var skillsInnerHTML='';
