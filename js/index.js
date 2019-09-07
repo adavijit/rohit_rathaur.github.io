b143fc5330dd051baa22f53dd6edb2b0032698a5
glob4lh3ll <54185582+glob4lh3ll@users.noreply.github.com>
2019-09-07 19:54:15 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -190,7 +190,7 @@ if(value===true) {
         }
     }).then((value)=>{
         if(value===true) { 
-            window.open('/Resume_Rohit_Singh_Rathaur.pdf');
+            window.open('/doc/Resume_Rohit_Singh_Rathaur.pdf');
         }
     })
 }
